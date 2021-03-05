d6a42849e967dec85ad57595c270896cbfc357d2
crugas <corey.schmidt@uga.edu>
2021-03-05 11:32:34 -0500

    Logic update to capture item file level, cleanup code

--- check_file_levels.py
+++ check_file_levels.py
@@ -6,7 +6,8 @@ import logging
 import csv
 
 
-def check_aos(date):
+def check_aos():
+    ao_instances = {}
     repos = client.get("repositories").json()
     for repo in repos:
         print("Repository: {}".format(repo["name"]))
@@ -17,73 +18,54 @@ def check_aos(date):
             get_ao = client.get(repo["uri"] + f"/archival_objects/{ao_id}")
             ao = json.loads(get_ao.text)
             if "level" in ao:
-                if "file" == ao["level"]:
+                if "file" == ao["level"] or "item" == ao["level"]:
                     if "parent" in ao:
                         check_parent = client.get(ao["parent"]["ref"]).json()
                         resource = client.get(ao["resource"]["ref"]).json()
-                        if check_parent["level"] == "file":
-                            if check_parent["instances"]:
-                                if check_parent["instances"][0]["sub_container"]:
-                                    if "indicactor_" not in check_parent["instances"][0]["sub_container"]:
-                                        try:
-                                            if not ao["dates"]:
-                                                write_csv(date, "a", ao["title"], "N/A", ao["uri"],
-                                                          resource["title"], resource["id_0"], repo["name"])
-                                                logging.info(
-                                                    "Repository: {}. Title: {}. Date: {}. URI: {}. Collection: {}".format(
-                                                        repo["name"],
-                                                        ao["title"],
-                                                        "N/A",
-                                                        ao["uri"],
-                                                        resource["title"]))
-                                            else:
-                                                write_csv(date, "a", ao["title"], ao["dates"][0]["expression"],
-                                                          ao["uri"],
-                                                          resource["title"], resource["id_0"], repo["name"])
-                                                logging.info(
-                                                    "Repository: {}. Title: {}. Date: {}. URI: {}. Collection: {}".format(
-                                                        repo["name"],
-                                                        ao["title"],
-                                                        ao["dates"][0]["expression"],
-                                                        ao["uri"],
-                                                        resource["title"]))
-                                            print(ao["title"])
-                                        except Exception as e:
-                                            print(f"Error writing archival object to log: {e}; {ao}")
-                                            logging.error(f"ERROR: {e}; {ao}")
-                            else:
-                                try:
-                                    if not ao["dates"]:
-                                        write_csv(date, "a", ao["title"], "N/A", ao["uri"],
-                                                  resource["title"], resource["id_0"], repo["name"])
-                                        logging.info(
-                                            "Repository: {}. Title: {}. Date: {}. URI: {}. Collection: {}".format(
-                                                repo["name"],
-                                                ao["title"],
-                                                "N/A",
-                                                ao["uri"],
-                                                resource["title"]))
-                                    else:
-                                        write_csv(date, "a", ao["title"], ao["dates"][0]["expression"], ao["uri"],
-                                                  resource["title"], resource["id_0"], repo["name"])
-                                        logging.info(
-                                            "Repository: {}. Title: {}. Date: {}. URI: {}. Collection: {}".format(
-                                                repo["name"],
-                                                ao["title"],
-                                                ao["dates"][0]["expression"],
-                                                ao["uri"],
-                                                resource["title"]))
-                                    print(ao["title"])
-                                except Exception as e:
-                                    print(f"Error writing archival object to log: {e}; {ao}")
-                                    logging.error(f"ERROR: {e}; {ao}")
+                        try:
+                            if check_parent["level"] == "file" or check_parent["level"] == "item":
+                                if check_parent["instances"]:
+                                    if check_parent["instances"][0]["sub_container"]:
+                                        if "indicactor_" not in check_parent["instances"][0]["sub_container"]:
+                                            if check_parent["ref_id"] not in ao_instances:
+                                                print(check_parent["title"], check_parent["ref_id"])
+                                                if not check_parent["dates"]:
+                                                    ao_instances[check_parent["ref_id"]] = [check_parent["title"],
+                                                                                            "N/A", check_parent["uri"],
+                                                                                            resource["title"],
+                                                                                            resource["id_0"],
+                                                                                            repo["name"]]
+                                                else:
+                                                    ao_instances[check_parent["ref_id"]] = [check_parent["title"],
+                                                                                            check_parent["dates"][0]["expression"],
+                                                                                            check_parent["uri"],
+                                                                                            resource["title"],
+                                                                                            resource["id_0"],
+                                                                                            repo["name"]]
+                                else:
+                                    if check_parent["ref_id"] not in ao_instances:
+                                        print(check_parent["title"])
+                                        if not check_parent["dates"]:
+                                            ao_instances[check_parent["ref_id"]] = [check_parent["title"], "N/A",
+                                                                                    check_parent["uri"],
+                                                                                    resource["title"],
+                                                                                    resource["id_0"], repo["name"]]
+                                        else:
+                                            ao_instances[check_parent["ref_id"]] = [check_parent["title"],
+                                                                                    check_parent["dates"][0]["expression"],
+                                                                                    check_parent["uri"],
+                                                                                    resource["title"], resource["id_0"],
+                                                                                    repo["name"]]
+                        except Exception as e:
+                            logging.error(f"ERROR checking parent_ao: {e}. AO: {check_parent}")
         print("-" * 100)
+    return ao_instances
 
 
-def write_csv(date, mode, ao_title, ao_date, ao_uri, coll_title, coll_id, repo):
+def write_csv(date, mode, ao_refid, ao_title, ao_date, ao_uri, coll_title, coll_id, repo):
     with open(f"{date}as_ao_errors.csv", mode=mode, newline='', encoding='utf-8') as ao_log:
         file_write = csv.writer(ao_log, delimiter=",")
-        file_write.writerow([ao_title, ao_date, ao_uri, coll_title, coll_id, repo])
+        file_write.writerow([ao_refid, ao_title, ao_date, ao_uri, coll_title, coll_id, repo])
         ao_log.close()
 
 
@@ -98,9 +80,11 @@ while run is True:
         client = ASnakeClient(baseurl=as_api, username=as_username, password=as_password)
         client.authorize()
         print("Checking Archival Objects in ASpace...")
-        write_csv(date_filename, "w", "AO_TITLE", "AO_DATE", "AO_URI", "COLLECTION_TITLE", "COLLECTION_ID",
+        write_csv(date_filename, "w", "AO_REFID", "AO_TITLE", "AO_DATE", "AO_URI", "COLLECTION_TITLE", "COLLECTION_ID",
                   "REPOSITORY")
-        check_aos(date_filename)
+        ao_instances = check_aos()
+        for key, value in ao_instances.items():
+            write_csv(date_filename, "a", key, value[0], value[1], value[2], value[3], value[4], value[5])
         run = False
     except Exception as e:
         print(f"Please try again. ERROR: {e}")
