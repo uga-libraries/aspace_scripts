b85c39f0b9417a8faf3ec8c448be1bfd2cf62537
crugas <cschmidtuga@gmail.com>
2021-08-06 09:35:43 -0400

    Restructured into functions, need to fix no results error

--- get_uris.py
+++ get_uris.py
@@ -27,9 +27,9 @@ def gui():
             break
         if event == '_WRITE_DOS_':
             if not values['_DO_FILE_']:
-                psg.popup('Please select a digital objects file')
+                psg.popup_error('ERROR\nPlease select a digital objects file', font=("Roboto", 14), keep_on_top=True)
             elif not values['_DOTEMP_FILE_']:
-                psg.popup('Please select a digital object template')
+                psg.popup_error('ERROR\nPlease select a digital object template', font=("Roboto", 14), keep_on_top=True)
             else:
                 defaults['_DO_FILE_'] = values['_DO_FILE_']
                 defaults['_DOTEMP_FILE_'] = values['_DOTEMP_FILE_']
@@ -45,60 +45,106 @@ def write_digobjs(digobj_file, dotemp_file, client, repo, gui_window):
     digobj_sheet = digobj_wb.active
     dotemp_wb = load_workbook(dotemp_file)
     dotemp_sheet = dotemp_wb.active
-    columns = [4, 6, 7, 8, 9, 10]
     write_row_index = 6
     total_digobjs = 0
+    digobj_columns = {0: "digital_object_id", 2: "digital_object_title", 3: "file_version_file_uri",
+                      5: "date_1_expression", 8: "digital_object_publish"}
+    sheet_columns = {}
+    sheet_colnum = 0
+    for col in digobj_sheet.iter_cols(max_col=digobj_sheet.max_column, values_only=True):
+        sheet_columns[sheet_colnum] = col[0]
+        sheet_colnum += 1
+    for col_key, col_value in digobj_columns.items():
+        if sheet_columns[col_key] != col_value:
+            psg.popup_error("ERROR:\nDigital Object file columns do not match!\n\n"
+                            "Check to make sure you entered the correct file")
+            print("ERROR: Digital Object file columns do not match!\n\nCheck to make sure you entered the correct file")
+            close_wbs(digobj_wb, dotemp_wb)
+            gui_window[f'{"_WRITE_DOS_"}'].update(disabled=False)
+            return
     for row in digobj_sheet.iter_rows(min_row=2, values_only=True):
+        write_row_index += 1
         total_digobjs += 1
         digobj_id = row[0]
         digobj_title = row[2]
         digobj_url = row[3]
         digobj_date = row[5]
         digobj_publish = row[8]
-        search_archobjs = client.get_paged(f"/repositories/{repo}/search",
-                                           params={"q": f'title:"{digobj_title} , {digobj_date}"',
-                                                   "type": ['archival_object']})
-        search_results = []
-        for results in search_archobjs:
-            search_results.append(results)
-        if len(search_results) > 1:
-            multresults_layout = [[psg.Text(f'\n\nFound multiple options for\n{digobj_title}, {digobj_date}\n\n'
-                                            f'Choose one of the following:\n')],
-                                  [psg.Listbox([f'{results["title"]}' for results in search_results], size=(80, 5),
-                                               key="_ARCHOBJ_FILE_")],
-                                  [psg.Button(" SELECT ", key="_SELECT_ARCHOBJ_")]]
-            multresults_window = psg.Window("Multiple Results for Archival Object", multresults_layout)
-            selection = True
-            while selection is True:
-                multresults_event, multresults_values = multresults_window.Read()
-                if multresults_event == "_SELECT_ARCHOBJ_":
-                    for result in search_results:
-                        print(multresults_values["_ARCHOBJ_FILE_"])
-                        if result["title"] == multresults_values["_ARCHOBJ_FILE_"][0]:
-                            uri = result["uri"]
-                            resource_uri = result["resource"]
-                            selection = False
-                            multresults_window.close()
-                            break
-        else:
-            for result in search_results:
-                uri = result["uri"]
-                resource_uri = result["resource"]
-        column_map = {4: resource_uri, 6: uri, 7: digobj_id, 8: digobj_title, 9: digobj_publish, 10: digobj_url}
-        for column in columns:
-            dotemp_sheet.cell(row=write_row_index, column=column).value = column_map[column]
-        write_row_index += 1
-        print(column_map)
-        try:
-            dotemp_wb.save(dotemp_file)
-        except Exception as e:
-            print(f'\n\nFailed opening {dotemp_file}. Please close the record before trying again.\nError: {e}')
-            gui_window[f'{"_WRITE_DOS_"}'].update(disabled=False)
-            return
+        archobj_uri, resource_uri = get_results(client, repo, digobj_title, digobj_date)
+        if archobj_uri is not None and resource_uri is not None:
+            write_obj_error = write_digobj(resource_uri, archobj_uri, digobj_id, digobj_title, digobj_publish, digobj_url,
+                                           dotemp_sheet, write_row_index, dotemp_wb, dotemp_file, gui_window)
+            if write_obj_error is not None:
+                print(write_obj_error)
+                close_wbs(digobj_wb, dotemp_wb)
+                return
+    close_wbs(digobj_wb, dotemp_wb)
+    print(f'\n{"*" * 112}\n{" " * 40}Finished writing {total_digobjs} to {dotemp_sheet}\n{"*" * 112}')
+    gui_window[f'{"_WRITE_DOS_"}'].update(disabled=False)
+
+
+def close_wbs(digobj_wb, dotemp_wb):
     digobj_wb.close()
     dotemp_wb.close()
-    print(f'\n{"*"*112}\n{" "*40}Finished writing {total_digobjs} to {dotemp_sheet}\n{"*"*112}')
-    gui_window[f'{"_WRITE_DOS_"}'].update(disabled=False)
+
+
+def get_results(client, repo, digobj_title, digobj_date):
+    archobj_uri = None
+    resource_uri = None
+    search_archobjs = client.get_paged(f"/repositories/{repo}/search",
+                                       params={"q": f'title:"{digobj_title}, {digobj_date}"',
+                                               "type": ['archival_object']})
+    search_results = []
+    for results in search_archobjs:
+        search_results.append(results)
+    if len(search_results) > 1:
+        multresults_layout = [[psg.Text(f'\n\nFound multiple options for\n{digobj_title}, {digobj_date}\n\n'
+                                        f'Choose one of the following:\n')],
+                              [psg.Listbox([f'{results["title"]}' for results in search_results], size=(80, 5),
+                                           key="_ARCHOBJ_FILE_")],
+                              [psg.Button(" SELECT ", key="_SELECT_ARCHOBJ_")]]
+        multresults_window = psg.Window("Multiple Results for Archival Object", multresults_layout)
+        selection = True
+        while selection is True:
+            multresults_event, multresults_values = multresults_window.Read()
+            if multresults_event == "_SELECT_ARCHOBJ_":
+                for result in search_results:
+                    if result["title"] == multresults_values["_ARCHOBJ_FILE_"][0]:
+                        archobj_uri = result["uri"]
+                        resource_uri = result["resource"]
+                        selection = False
+                        multresults_window.close()
+                        break
+                    else:
+                        psg.popup_error("ERROR\nSelected result does not match!", font=("Roboto", 14),
+                                        keep_on_top=True)
+    elif len(search_results) is 0:
+        print(f'ERROR:\nNo results found.\n\nNot writing {digobj_title} to file...')
+        return archobj_uri, resource_uri
+    else:
+        for result in search_results:
+            archobj_uri = result["uri"]
+            resource_uri = result["resource"]
+    if archobj_uri is None and resource_uri is None:
+        print(f'{digobj_title}, {digobj_date}')
+        print(search_results)
+    return archobj_uri, resource_uri
+
+
+def write_digobj(resource_uri, archobj_uri, digobj_id, digobj_title, digobj_publish, digobj_url, dotemp_sheet,
+                 write_row_index, dotemp_wb, dotemp_file, gui_window):
+    column_map = {4: resource_uri, 6: archobj_uri, 7: digobj_id, 8: digobj_title, 9: digobj_publish, 10: digobj_url}
+    for column_num, column_value in column_map.items():
+        dotemp_sheet.cell(row=write_row_index, column=column_num).value = column_value
+    write_row_index += 1
+    # print(f'Wrote {digobj_title} to {dotemp_file}\n')
+    try:
+        dotemp_wb.save(dotemp_file)
+        return None
+    except Exception as e:
+        error = f'\n\nFailed opening {dotemp_file}. Please close the record before trying again.\nError: {e}'
+        gui_window[f'{"_WRITE_DOS_"}'].update(disabled=False)
+        return error
 
 
 def get_aspace_log(defaults):
@@ -108,7 +154,7 @@ def get_aspace_log(defaults):
     uses ASnake.client to authenticate and stay connected to ArchivesSpace. Documentation for ASnake can be found here:
     https://archivesspace-labs.github.io/ArchivesSnake/html/index.html
     Args:
-        defaults (UserSetting class): contains the data from defaults.json file, all data the user has specified as default
+        defaults (UserSetting class): contains data from defaults.json file, all data the user has specified as default
     Returns:
         close_program (bool): if a user exits the popup, this will return true and end run_gui()
         connect_client (ASnake.client object): the ArchivesSpace ASnake client for accessing and connecting to the API
@@ -168,4 +214,5 @@ def get_aspace_log(defaults):
     return close_program, connect_client, repositories
 
 
-gui()
+if __name__ == "__main__":
+    gui()
