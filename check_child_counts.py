694b1e06888f8e90263c718c2fbcea4c3d08b347
crugas <cschmidtuga@gmail.com>
2021-10-14 11:19:45 -0400

    Fixed writing to CSV

--- check_child_counts.py
+++ check_child_counts.py
@@ -17,12 +17,14 @@ def check_child_counts(tree_info, child_counts, aspace_coll_id):
     if tree_info["child_count"] >= 1000 and tree_info["uri"] not in child_counts:
         child_counts[f"{tree_info['uri']}"] = (tree_info["title"], tree_info["child_count"], tree_info["level"],
                                                aspace_coll_id)
+        print(aspace_coll_id)
     if tree_info["precomputed_waypoints"]:
         for waypoint_num, waypoint_info in tree_info["precomputed_waypoints"][""].items():
             for child in waypoint_info:
                 if child["child_count"] >= 1000:
                     child_counts[f'{child["uri"]}'] = (child["title"], child["child_count"], child["level"],
                                                        aspace_coll_id)
+                    print(aspace_coll_id)
                 if "precomputed_waypoints" in child:
                     check_child_counts(child, child_counts, aspace_coll_id)
     return child_counts
@@ -46,14 +48,15 @@ for repo in repos:
         if resource.json()["publish"] is True:
             if resource.status_code == 200:
                 tree_info = client.get(f'/repositories/{repo_id}/resources/{resource_id}/tree/root').json()
-                child_counts = check_child_counts(tree_info, child_counts, combined_aspace_id_clean)
+                child_counts = check_child_counts(tree_info, child_counts, combined_id)
 
 new_dict = json.dumps(child_counts)
 load_dict = json.loads(new_dict)
-with open("data/check_child_count.csv", "w", encoding="utf8") as file:
+with open("data/check_child_count.csv", "w", encoding="utf8", newline='') as file:
     writer = csv.writer(file)
-    for child, data in load_dict.items():  # TODO: not working - separating each character
-        writer.writerow(str(child) + ', ' + str(data[0]) + ', ' + str(data[1]) + ', ' + str(data[2]) + ', ' + str(data[3]))
+    for child, data in load_dict.items():
+        fields = [str(child), str(data[0]), str(data[1]), str(data[2]), str(data[3])]
+        writer.writerow(fields)
     file.close()
 
 
